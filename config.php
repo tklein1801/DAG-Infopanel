8a74521609518a93d05f7623b8504234e2d43884
Thorben <38816229+tklein1801@users.noreply.github.com>
2020-08-31 01:17:02 +0200

    :wrench: Update config.php & implement login

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 
 // URL's
-$url_host = "http://localhost/";
+$url_host = "https://info.dulliag.de/";
 
 // Directories
 $dir_assets = __DIR__ . "/assets/";
@@ -10,6 +11,7 @@ $dir_images = $url_host . "assets/img/";
 $dir_js = $url_host . "assets/js/";
 $dir_plugins = $url_host . "plugins/";
 $dir_components = $dir_assets . "components/";
+$dir_api = $url_host . "API/";
 
 // Components
 $comp_navbar = $dir_components . "navbar.php";
@@ -19,3 +21,10 @@ $comp_footer = $dir_components . "footer.php";
 // Other
 $other_theme_color = "#007bff";
 $other_logo = $dir_images . "logo.jpg";
+
+// Check if the user is logged in
+if ($_SERVER['REQUEST_URI'] !== "/Login/") {
+  if (!isset($_SESSION['login'])) {
+    header("Location: " . $url_host . "Login/");
+  }
+}
